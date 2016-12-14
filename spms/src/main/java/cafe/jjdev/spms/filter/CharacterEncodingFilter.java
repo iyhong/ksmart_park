2294bdc6c8adc576ffc7ebb0cf6444b4fb27963c
Administrator <Administrator@smart20103>
2016-12-14 13:53:20 +0900

    list servlet 생성

---
+++ spms/src/main/java/cafe/jjdev/spms/filter/CharacterEncodingFilter.java
@@ -0,0 +1,35 @@
+package cafe.jjdev.spms.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+@WebFilter(
+	urlPatterns="/*",
+	initParams={
+		@WebInitParam(name="encoding",value="euc-kr")
+	})
+public class CharacterEncodingFilter implements Filter{
+	FilterConfig config;
+	
+	public void init(FilterConfig config) throws ServletException {
+		this.config = config;
+	}
+	
+	public void doFilter(
+			ServletRequest request, ServletResponse response,
+			FilterChain nextFilter) throws IOException, ServletException {
+		System.out.println("CharacterEncodingFilter ½ÇÇà...");
+		request.setCharacterEncoding(config.getInitParameter("encoding"));
+		nextFilter.doFilter(request, response);
+	}
+
+	public void destroy() {}
+}
