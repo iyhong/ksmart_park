1e7527c055814cc8ab34e824d6cf08354e207a96
Administrator <Administrator@smart20103>
2016-12-14 14:12:37 +0900

    list servlet 과 list jsp 추가.. 서블릿과 뷰 분리작업

--- spms/src/main/java/cafe/jjdev/spms/servlets/MemberListServlet.java
+++ spms/src/main/java/cafe/jjdev/spms/servlets/MemberListServlet.java
@@ -1,23 +1,24 @@
 package cafe.jjdev.spms.servlets;
-
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
 import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import cafe.jjdev.spms.vo.Member;
 
-
-// ServletContext에 보관된 Connection 객체 사용  
+// UI 출력 코드를 제거하고, UI 생성 및 출력을 JSP에게 위임한다.
 @WebServlet("/member/list")
 public class MemberListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -32,19 +33,22 @@ public class MemberListServlet extends HttpServlet {
 
 		try {
 			ServletContext sc = this.getServletContext();
-			conn = (Connection) sc.getAttribute("conn"); 
+			Class.forName(sc.getInitParameter("driver"));
+			conn = DriverManager.getConnection(
+						sc.getInitParameter("url"),
+						sc.getInitParameter("username"),
+						sc.getInitParameter("password")); 
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery(
 					"SELECT MNO,MNAME,EMAIL,CRE_DATE" + 
 					" FROM MEMBERS" +
 					" ORDER BY MNO ASC");
 			
-			response.setContentType("text/html; charset=euc-kr");
+			response.setContentType("text/html; charset=UTF-8");
 			ArrayList<Member> members = new ArrayList<Member>();
 			
 			// 데이터베이스에서 회원 정보를 가져와 Member에 담는다.
 			// 그리고 Member객체를 ArrayList에 추가한다.
-			//set메서드 리턴값이 자기자신이어서 아래처럼 setting 가능하다.
 			while(rs.next()) {
 				members.add(new Member()
 							.setNo(rs.getInt("MNO"))
@@ -62,15 +66,12 @@ public class MemberListServlet extends HttpServlet {
 			rd.include(request, response);
 			
 		} catch (Exception e) {
-			e.printStackTrace();
-			request.setAttribute("error", e);
-			RequestDispatcher rd = request.getRequestDispatcher("/Error.jsp");
-			rd.forward(request, response);
+			throw new ServletException(e);
 			
 		} finally {
 			try {if (rs != null) rs.close();} catch(Exception e) {}
 			try {if (stmt != null) stmt.close();} catch(Exception e) {}
-			//try {if (conn != null) conn.close();} catch(Exception e) {}
+			try {if (conn != null) conn.close();} catch(Exception e) {}
 		}
 
 	}
